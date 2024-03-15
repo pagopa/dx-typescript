5b95a5ab444c7c4cf68dc0c39c50f57c8d7226e4
Daniele Manni <dan.manni@gmail.com>
2024-03-15 15:46:08 +0100

    [#DEVEX-60] Fix ESLint configuration

--- templates/monorepo/apps/azure-functions-api/.eslintrc.js
+++ templates/monorepo/apps/azure-functions-api/.eslintrc.js
@@ -1,24 +1,33 @@
+const { resolve } = require("node:path");
+const currentDirectory =  process.cwd();
+const project = currentDirectory.includes("templates/monorepo") ? resolve(process.cwd(), "tsconfig.json") : resolve(process.cwd(), "templates/monorepo/tsconfig.json");
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "ignorePatterns": [
-        "node_modules",
-        "src/generated",
-        "*.d.ts",
-        "jest.config.js"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "project": "tsconfig.json",
-        "sourceType": "module"
-    },
-    "extends": [
-        "@pagopa/eslint-config-node/index",
-    ],
-    "rules": {
-        "@typescript-eslint/consistent-type-definitions": ["error", "type"]
-    }
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "ignorePatterns": [
+    "node_modules",
+    "src/generated",
+    "*.d.ts",
+    "jest.config.js"
+  ],
+  root: true,
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": { "project": [project] },
+  "extends": [
+    "@pagopa/eslint-config-node/index",
+  ],
+  "rules": {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }
+    ]
+  }
 }
--- templates/monorepo/apps/azure-functions-api/src/main.ts
+++ templates/monorepo/apps/azure-functions-api/src/main.ts
@@ -1,4 +1,5 @@
 import { CosmosClient } from "@azure/cosmos";
+import { app } from "@azure/functions";
 import { getConfigOrThrow } from "./config";
 import { InfoFn } from "./functions/info";
 import { CreateRedisClientSingleton } from "./utils/redis/client";
@@ -11,5 +12,18 @@ const database = cosmosClient.database(config.COSMOS_DB_NAME);
 
 const redisClientTask = CreateRedisClientSingleton(config);
 
-export const Info = InfoFn({ db: database, redisClientTask });
-export const GenerateNonce = GenerateNonceFn({ redisClientTask });
+const Info = InfoFn({ db: database, redisClientTask });
+app.http("Info", {
+  authLevel: "anonymous",
+  handler: Info,
+  methods: ["GET"],
+  route: "info"
+});
+
+const GenerateNonce = GenerateNonceFn({ redisClientTask });
+app.http("Info", {
+  authLevel: "function",
+  handler: GenerateNonce,
+  methods: ["GET"],
+  route: "api/v1/nonce/generate"
+});
