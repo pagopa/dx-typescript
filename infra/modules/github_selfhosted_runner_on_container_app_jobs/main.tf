06dcd6affc1fcfcf6f01cf5abea4b83665152926
Andrea Grillo <andrea.grillo@pagopa.it>
2024-04-22 09:35:59 +0200

    fix name

--- infra/modules/github_selfhosted_runner_on_container_app_jobs/main.tf
+++ infra/modules/github_selfhosted_runner_on_container_app_jobs/main.tf
@@ -26,7 +26,7 @@ module "container_app_job_selfhosted_runner" {
   }
 
   job = {
-    name = replace(replace(var.repo_name, var.prefix, ""), var.env_short, "")
+    name = replace(replace(var.repo_name, "${var.prefix}-", ""), "${var.env_short}-", "")
     repo = var.repo_name
   }
 
