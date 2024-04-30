9f2730f7020c8d19d23cdbba25a0a471e7a3abe8
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2024-04-30 19:35:57 +0200

    [#DEVEX-60] Azure Function API template workspace (#4)

---
+++ apps/azure-functions-api/src/main.ts
@@ -0,0 +1,20 @@
+import { app } from "@azure/functions";
+import { getConfigOrError } from "./config";
+import { InfoFn } from "./functions/info";
+import { pipe } from "fp-ts/lib/function";
+import * as E from "fp-ts/Either";
+
+const _config = pipe(
+  getConfigOrError(),
+  E.getOrElseW((error) => {
+    throw error;
+  }),
+);
+
+const Info = InfoFn({});
+app.http("Info", {
+  authLevel: "anonymous",
+  handler: Info,
+  methods: ["GET"],
+  route: "info",
+});
